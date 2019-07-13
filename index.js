4cc33049271ccb8db524dbfc44b7925ead5c078a
Ngamjit Louichamwun <fulllove244@gmail.com>
2019-07-13 14:15:14 +0700

    เพิ่มหน้าเทล

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('TatEiEii'))
 app.get('/about', (req, res) => res.send('หน้า about'))
-app.get('/contact', (req, res) => res.send('หน้าเกี่ยวกับ'))
+app.get('/contact', (req, res) => res.send('หน้า ติดต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
